d18cede861bdb1fc0faff1016478ef46daad475c
Jun87 <g.jyj87@gmail.com>
2023-04-24 14:36:14 +0900

    댓글 추가 삭제 로직

---
+++ src/main/java/com/study/RestApiTestController.java
@@ -0,0 +1,23 @@
+package com.study;
+
+import com.study.common.dto.SearchDto;
+import com.study.common.paging.PagingResponse;
+import com.study.domain.post.PostResponse;
+import com.study.domain.post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RestController가 붙은 컨트롤러의 모든 메서드에는 자동으로 @ResponseBody가 적용된다고 이해해 주시면 됩니다.
+@RequiredArgsConstructor
+public class RestApiTestController {
+
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public PagingResponse<PostResponse> findAllPost() {
+        return postService.findAllPost(new SearchDto());
+    }
+
+}
